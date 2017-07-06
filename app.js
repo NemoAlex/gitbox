a4b990fc376da7e32e168dff073e4d553d6ce6e2
Nemo <zhhjchina@gmail.com>
2017-07-06 12:28:28 +0800

    Use leancloud instead of stormpath

--- app.js
+++ app.js
@@ -9,45 +9,85 @@ var express = require('express')
   , rimraf = require('rimraf')
   , util = require('./lib/util')
   , git = require('./lib/git')
-  , stormpath = require('express-stormpath')
+  , AV = require('leancloud-storage')
   , githubhook = require('githubhook')
+  , cookieParser = require('cookie-parser')
 
-app.use(express.static(Path.join(__dirname, 'static')));
+app.use(express.static(Path.join(__dirname, 'static')))
+app.use(cookieParser())
+app.post('*', bodyParser.json(), bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'jade')
 
-if (config.enableStormpath) {
-  app.use(stormpath.init(app, {
-    client: {
-      apiKey: {
-        id: config.stormpath.id,
-        secret: config.stormpath.secret,
-      }
-    },
-    application: {
-      href: config.stormpath.href
-    },
-    web: {
-      register: {
-        enabled: config.enableRegister
-      }
-    }
-  }))
+if (config.enableRegister) {
+  app.get('/sign_up', (req, res) => {
+    res.render('sign_up.jade', {
+      projectName: config.projectName
+    })
+  })
 
-  app.use(stormpath.loginRequired)
+  app.post('/sign_up', (req, res) => {
+    var user = new AV.User()
+    user.setUsername(req.body.username)
+    user.setPassword(req.body.password)
+    user.signUp().then(function (loginedUser) {
+      return res.redirect('/')
+    }, function (error) {
+      return res.redirect('/sign_up')
+    })
+  })
+}
+
+if (config.requireLogin) {
+  AV.init(config.leancloud)
+
+  app.get('/login', (req, res) => {
+    res.render('login.jade', {
+      config
+    })
+  })
+
+  app.post('/login', (req, res) => {
+    AV.User.logIn(req.body.username, req.body.password)
+    .then(result => {
+      res.cookie('authentication', result._sessionToken, { maxAge: 900000, httpOnly: true })
+      return res.redirect('/')
+    })
+    .catch(err => {
+      return res.redirect('/login')
+    })
+  })
+
+  app.get('/logout', (req, res) => {
+    res.cookie('authentication', '')
+    res.redirect('/')
+  })
+
+  app.use((req, res, next) => {
+    var token = req.cookies.authentication
+    if (!token) return res.redirect('/login')
+    AV.User.become(token)
+    .then(loginedUser => {
+      req.authentication = { user: loginedUser.get('username') }
+      next()
+    }).catch(err => {
+      req.redirect('/login')
+    })
+  })
 }
 
 app.get('/', (req, res) => {
   res.render('index.jade', {
-    projectName: config.projectName
+    config,
+    username: req.authentication ? req.authentication.user : '匿名用户'
   })
-});
+})
 
 app.get('/tree', (req, res) => {
-  var root = config.diskPath;
-  var tree = dirTree(root, ['^[_|.]']);
+  var root = config.diskPath
+  var tree = dirTree(root, ['^[_|.]'])
   res.json({root, tree})
-});
+})
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -61,33 +101,33 @@ var upload = multer({ storage: storage })
 
 app.post('/upload', upload.any(), (req, res) => {
   res.end()
-  git.updateToOrigin('Update by ' + req.user.fullName)
-});
+  git.updateToOrigin('Update by ' + req.authentication ? req.authentication.user : '匿名用户')
+})
 
-app.post('/create_folder', bodyParser.json(), (req, res) => {
+app.post('/create_folder', (req, res) => {
   var path = req.body.path
   fs.mkdirSync(config.diskPath + path)
 
   res.end()
 })
 
-app.post('/write_file', bodyParser.json(), (req, res) => {
+app.post('/write_file', (req, res) => {
   var path = req.body.path
     , content = req.body.content
 
-  fs.writeFileSync(config.diskPath + path, content);
+  fs.writeFileSync(config.diskPath + path, content)
   res.end()
-  git.updateToOrigin('Update by ' + req.user.fullName)
+  git.updateToOrigin('Update by ' + req.authentication ? req.authentication.user : '匿名用户')
 })
 
-app.get('/read_file', bodyParser.json(), (req, res) => {
+app.get('/read_file', (req, res) => {
   var path = req.query.path
 
-  var content = fs.readFileSync(config.diskPath + path, 'UTF-8');
+  var content = fs.readFileSync(config.diskPath + path, 'UTF-8')
   res.end(content)
 })
 
-app.post('/rename', bodyParser.json(), (req, res) => {
+app.post('/rename', (req, res) => {
   var path = config.diskPath + req.body.path
   var newname = req.body.newname
   var arr = path.split('/')
@@ -97,19 +137,19 @@ app.post('/rename', bodyParser.json(), (req, res) => {
   fs.renameSync(path, newPath)
 
   res.end()
-  git.updateToOrigin('Update by ' + req.user.fullName)
+  git.updateToOrigin('Update by ' + req.authentication ? req.authentication.user : '匿名用户')
 })
 
-app.post('/remove', bodyParser.json(), (req, res) => {
+app.post('/remove', (req, res) => {
   var path = config.diskPath + req.body.path
   rimraf(path, function () {
     res.end()
-    git.updateToOrigin('Update by ' + req.user.fullName)
+    git.updateToOrigin('Update by ' + req.authentication ? req.authentication.user : '匿名用户')
   })
 })
 
 app.get('/commit_and_push', (req, res) => {
-  git.updateToOrigin('Update by ' + req.user.fullName)
+  git.updateToOrigin('Update by ' + req.authentication ? req.authentication.user : '匿名用户')
   .then(function (log) {
     res.json(log)
   })
